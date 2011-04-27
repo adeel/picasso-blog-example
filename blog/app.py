ea03097758a5fd8569b22ab337fe6a51d6711fa5
Adeel Ahmad Khan <adeel@adeel.ru>
2011-04-26 21:42:10 -0400

    Add an auth controller and basic login.

--- blog/app.py
+++ blog/app.py
@@ -1,12 +1,21 @@
 from picasso import *
 
+from blog.controllers import auth
 from blog.controllers import posts
 
-routes = setup_routes(
+auth_routes = setup_routes(
+  GET("/login", auth.login),
+  POST("/login", auth.login_post))
+
+post_routes = setup_routes(
   GET("/", posts.index),
   GET("/new", posts.new),
   POST("/new", posts.save),
-  GET("/posts/:hash", posts.view),
+  GET("/posts/:hash", posts.view))
+
+routes = setup_routes(
+  auth_routes,
+  post_routes,
   routing.not_found("<h1>Not Found</h1>"))
 
 app = setup_app(routes, {"views": {"template_dir": "blog/views"}})
---
+++ blog/controllers/auth.py
@@ -0,0 +1,15 @@
+from blog.controllers import *
+from blog import config
+
+def login(req):
+  return "auth/login", {}
+
+def login_post(req):
+  print req["params"]
+  password = req["params"].get("password")
+  if password and password == config.password:
+    req["session"]["is_logged_in"] = True
+    return redirect("/")
+  else:
+    req["flash"]["error"] = "Wrong password."
+    return login(req)
